9588fdb203df15671fa2f622db8a8bafa6207f70
HerbertNtim <herbertntim15@gmail.com>
2025-12-06 21:57:06 +0000

    completed left and right movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,6 +24,7 @@ class AlienInvasion:
     while True:
       # Watch for keyboard and mouse events.
       self._check_events()
+      self.ship.update()
       self._update_screen()
   
   def _check_events(self):
@@ -34,9 +35,13 @@ class AlienInvasion:
         if event.key == pygame.K_RIGHT:
           # Move the ship to the right
           self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+          self.ship.moving_left = True
       elif event.type == pygame.KEYUP:
         if event.key == pygame.K_RIGHT:
           self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+          self.ship.moving_left = False
 
   def _update_screen(self):
     # Redraw the screen during each pass through the loop.
