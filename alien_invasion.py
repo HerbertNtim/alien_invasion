b4577e15e629f96767df7df82d268bc2b790c938
HerbertNtim <herbertntim15@gmail.com>
2025-12-08 18:05:04 +0000

    working on bullet

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullets
 
 class AlienInvasion:
   """Overall Class to manage game assets and behavior."""
@@ -17,6 +18,7 @@ class AlienInvasion:
     self.settings.screen_height = self.screen.get_rect().height
     pygame.display.set_caption("Alien Invasion")
     self.ship = Ship(self)
+    self.bullets = pygame.sprite.Group()
 
     # Set the background color.
     self.bg_color = (self.settings.bg_color)
@@ -28,6 +30,7 @@ class AlienInvasion:
       self._check_events()
       self.ship.update()
       self._update_screen()
+      self.bullets.update()
   
   def _check_events(self):
     """Respond to keypress and mouse events"""
@@ -52,6 +55,8 @@ class AlienInvasion:
       self.ship.moving_down = True
     elif event.key == pygame.K_q:
       sys.exit()
+    elif event.type == pygame.K_SPACE:
+      self._fire_bullet()
 
   def _check_keyup_events(self, event):
     """Respond to key releases"""
@@ -64,10 +69,17 @@ class AlienInvasion:
     elif event.key == pygame.K_DOWN:
       self.ship.moving_down = False
 
+  def _fire_bullet(self):
+    """Create a new bullet and add it to the bullets group."""
+    new_Bullet = Bullets(self)
+    self.bullets.add(new_Bullet)
+
   def _update_screen(self):
     # Redraw the screen during each pass through the loop.
     self.screen.fill(self.bg_color)
     self.ship.blitme()
+    for bullet in self.bullets.sprites():
+      bullet.draw_bullet()
     # Make the most recently drawn screen visible.
     pygame.display.flip()
 
--- settings.py
+++ settings.py
@@ -6,6 +6,12 @@ class Settings:
     self.screen_width = 1200
     self.screen_height = 800
     self.bg_color = (128, 128, 128)
+    
+    # Bullet Settings
+    self.bullet_speed = 1.0
+    self.bullet_width = 3
+    self.bullet_height = 15
+    self.bullet_color = (60, 60, 60)
 
     # Ship settings
     self.ship_speed = 1.5
