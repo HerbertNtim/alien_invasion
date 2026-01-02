e066f8e02da1fbb7d74804e7a981f6076c9ce9ac
HerbertNtim <herbertntim15@gmail.com>
2026-01-02 20:31:29 +0000

    removed spaces and created star file

--- ship.py
+++ ship.py
@@ -10,19 +10,15 @@ class Ship(Sprite):
     self.screen = ai_game.screen
     self.settings = ai_game.settings
     self.screen_rect = ai_game.screen.get_rect()
-
     # Load the ship image and get its rect.
     self.image = pygame.image.load("images/ship.PNG").convert_alpha()
     self.image = pygame.transform.scale(self.image, (80, 60))
-    self.rect = self.image.get_rect()
-    
+    self.rect = self.image.get_rect()  
     # Start each new ship at the bottom center of the screen.
     self.rect.midbottom = self.screen_rect.midbottom
-
     # Store a decimal value for the ship's horizontal position.
     self.x = float(self.rect.x)
     self.y = float(self.rect.y)
-
     # Moving flag
     self.moving_right = False
     self.moving_left = False
@@ -40,7 +36,6 @@ class Ship(Sprite):
       self.y -= self.settings.ship_speed
     if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
       self.y += self.settings.ship_speed
-
     # Update rect object from self.x
     self.rect.x = self.x
     self.rect.y = self.y
